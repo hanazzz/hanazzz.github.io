ccb3b5cb828e97aeb48ebe2f7ce76974980640f6
Hana Zait <hana.o.zait@gmail.com>
2022-11-13 18:35:13 -0800

    Add EmailJS script tags and update contact form fields accordingly. Add footer.

---
+++ script.js
@@ -0,0 +1,26 @@
+// Send email from contact form
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      let formConfirmation = undefined;
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(function() {
+              console.log('SUCCESS!');
+              formConfirmation = "Message sent! Check your email for a confirmation."
+          }, function(error) {
+              console.log('FAILED...', error);
+              formConfirmation = "Uh oh, something went wrong. Please wait and try again or message me on LinkedIn instead."
+          })
+          .then(() => {
+              function toggleConfirmation() {
+                  document.getElementById("contact-confirmation").classList.toggle("hidden");
+              }
+              // Show confirmation message
+              toggleConfirmation();
+              // Update confirmation text
+              document.getElementById("contact-confirmation").innerText = formConfirmation;
+              // Hide confirmation message after 10 seconds
+              // setTimeout(toggleConfirmation, 10000)
+          });
+  });
+}
