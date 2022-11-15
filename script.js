3bace4a51447a67da81ea24c33bd47c2c4b2fd22
Hana Zait <hana.o.zait@gmail.com>
2022-11-15 15:07:14 -0800

    Add typing effect for displaying intro facts

--- script.js
+++ script.js
@@ -44,20 +44,72 @@ const funFacts = [
   'passionate about writing clear and thorough documentation.',
 ]
 
-let factIdx = 0
-
 // Display different facts about me with button click
+let factIndex = 0
+
 function getFact() {
-  // if factIdx greater than funFacts --> at end of array --> reset factIdx to 0
-  if (factIdx === funFacts.length) {
-    factIdx = 0
+  // if factIndex greater than funFacts --> at end of array --> reset factIndex to 0
+  if (factIndex === funFacts.length) {
+    factIndex = 0
   }
   // Get fact from array using idx
-  let currentFact = funFacts[factIdx]
+  let currentFact = funFacts[factIndex]
   // Update DOM
   document.getElementById('intro-blurb').innerText = currentFact
   // Increase idx
-  factIdx += 1;
+  factIndex += 1;
 }
 
 document.getElementById('intro-button').addEventListener('click', getFact);
+
+
+// Typing different facts about myself
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const typingDelay = 100; // Typing speed
+const erasingDelay = 100; // Erasing speed
+const newTextDelay = 2000; // Delay between current and next text
+
+let factIndex2 = 0;
+let charIndex = 0;
+
+function type() {
+  // If charIndex < length of current str --> there are chars left in the str to type
+  // --> display one more char and increase charIndex
+  if (charIndex < funFacts[factIndex2].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += funFacts[factIndex2].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  // Otherwise, all chars are displayed --> start erasing chars
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  // If charIndex < 0 --> there are chars left in the str to erase
+  // --> display one less char and decrease charIndex
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = funFacts[factIndex2].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  // Otherwise, entire str has been erased
+  // --> increase factIndex2 and start typing next str
+  else {
+    cursorSpan.classList.remove("typing");
+    factIndex2++;
+    if(factIndex2>=funFacts.length) factIndex2=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+// On DOM Load initiate the typing effect
+document.addEventListener("DOMContentLoaded", function() {
+  if(funFacts.length) setTimeout(type, newTextDelay + 250);
+});
